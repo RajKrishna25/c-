f9a2a3ed1400e0137b9cfd523691939fb3a09a2c
RajKrishna25 <157878715+RajKrishna25@users.noreply.github.com>
2024-01-29 20:04:35 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+    //Interger Datatypes
+    int intergerValue = 20;
+    cout<<"Stored integer value is : - " << intergerValue<<endl;   
+
+    //Floating point Datatypes
+    float floatValue = 3.14;
+    cout<<"Stored float value is : - " <<floatValue<<endl;
+
+    //Character datatypes
+    char charValue = 'S';
+    cout<<"Stored Character value is : - " <<charValue<<endl;
+
+
+    // Boolean datatypes
+    bool boolValue = true;
+    cout<<"Stored boolean value is : - " <<boolValue<<endl;
+
+    return 0;
+
+}
