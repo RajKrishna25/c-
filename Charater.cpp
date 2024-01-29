71cf1a60680123230c870f00b91ab1b771843964
RajKrishna25 <157878715+RajKrishna25@users.noreply.github.com>
2024-01-29 20:02:28 +0530

    Add files via upload

---
+++ Charater.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    char Gender;
+    cout<<"Are you Male / Female? " << endl;
+    cin >> Gender;
+    cout << "You are " << Gender << endl;
+
+    return 0;
+
+}
