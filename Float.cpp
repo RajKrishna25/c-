6dd0115eb64a7fa693bc6f1e5658d2ff5cea5c01
RajKrishna25 <157878715+RajKrishna25@users.noreply.github.com>
2024-01-29 20:05:07 +0530

    Add files via upload

---
+++ Float.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    float Weight;
+    cout<<"What is your weight? " << endl;
+    cin >> Weight;
+    cout << "Your weight is " << Weight << endl;
+
+    return 0;
+
+}
